d6cd372627cd93cdd3b8a9046a8d939fa5e7cde4
svarathalingam <svarathalingam>
2019-11-18 21:16:22 +0800

    Part A Question 1

---
+++ src/Solution.java
@@ -0,0 +1,11 @@
+import com.java.assignment.Bird;
+
+public class Solution {
+    public static void main(String args[]){
+
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+    }
+}
